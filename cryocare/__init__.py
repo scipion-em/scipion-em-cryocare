966e3b173bcf0e4df38adf75dc8d3e653ee772f5
JorMaister <jorgejjs86@gmail.com>
2022-09-30 15:12:05 +0200

    Small fix III

--- cryocare/__init__.py
+++ cryocare/__init__.py
@@ -78,8 +78,8 @@ class Plugin(pwem.Plugin):
         installationCmd += 'conda activate %s && ' % CRYOCARE_ENV_NAME
 
         # Install cryoCARE
-        installationCmd += 'pip install "filelock<4,>=3.0.0" && '
-        installationCmd += 'pip install "six<2,>=1.9.0" && '
+        # installationCmd += 'pip install "filelock<4,>=3.0.0" && '
+        # installationCmd += 'pip install "six<2,>=1.9.0" && '
         installationCmd += 'pip install tensorflow-gpu==2.4 && '
         installationCmd += 'pip install %s==%s && ' % (CRYOCARE, CRYOCARE_DEFAULT_VERSION)
 
