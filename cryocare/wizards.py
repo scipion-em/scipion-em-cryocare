06905aff58d782c861ee0cda85a875a89c175ab4
JorMaister <jorgejjs86@gmail.com>
2024-02-14 10:16:05 +0100

    Remove residual references to deprecated protocol ProtCryoCAREPrepareTrainingData

--- cryocare/wizards.py
+++ cryocare/wizards.py
@@ -2,14 +2,12 @@ from pyworkflow.gui import ListTreeProviderString, dialog
 from pyworkflow.object import Integer
 from pyworkflow.wizard import Wizard
 
-from cryocare.protocols import ProtCryoCAREPrepareTrainingData
-
 PATCH_SHAPE = 'patch_shape'
 
 
 class cryoCAREPatchSizeWizard(Wizard):
     # Dictionary to target protocol parameters
-    _targets = [(ProtCryoCAREPrepareTrainingData, [PATCH_SHAPE])]
+    _targets = [PATCH_SHAPE]
 
     def show(self, form, *params):
         patchValues = [Integer(i) for i in range(32, 130, 8)]
